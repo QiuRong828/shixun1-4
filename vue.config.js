f01beebefc323b9e76dea769bf687b2c600d3d3c
Qiurong <3329279429@qq.com>
2022-07-11 16:24:52 +0800

    feat(request.js vue.config.js): axios二次封装  配置跨域

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: './',
+  devServer: {
+    open: true,
+    host: 'localhost',
+    https: false,
+    port: 8080,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_CROSS_ORIGIN,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
+  }
 })
